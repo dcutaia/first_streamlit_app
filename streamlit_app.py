6a0081e11b6fdb8b028d3ad42aec974f3ad21f7b
dcutaia <88193829+dcutaia@users.noreply.github.com>
2023-02-22 11:53:30 +0100

    Update streamlit_app.py
    removed double text

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,6 +70,3 @@ if streamlit.button('Add a Fruit to the List'):
   back_from_function = insert_row_snowflake(add_my_fruit)
   streamlit.text(back_from_function)
   
-
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
